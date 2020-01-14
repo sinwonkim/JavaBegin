bd7a7e1e29c83e6655b255848c660ba2c03d0910
user <user@LAPTOP-IL8J020J>
2020-01-14 14:29:28 +0900

    # first Commit

---
+++ JavaF/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main { // 중괄호를 블록이라 부르고 쌍으로 이루어져 있다. 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Main method 시작하는 지점 
+		// 대소문자를 가리키기 때문에 똑같이 적으셔야 한다.  
+		// 알고 싶은 내용을 화면에 출력을 한다. 괄호 사이에 적으면 다만 규칙이 있다. 글자라고 한다면  
+		System.out.println("JAVA 참 어렵다.");
+	}
+
+}
